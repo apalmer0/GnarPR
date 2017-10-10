2b6330ab740e68dac7bb9d9fd4f7ec54c5820189
Andrew Palmer <andrew.palmer.10@gmail.com>
2017-10-10 08:10:09 -0400

    Update app to use Rails API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   def verify_from_slack
     head :forbidden unless params[:token] == Rails.application.secrets.slack_api_key
